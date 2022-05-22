d8999f1f2c4d494fc758ead215a1f611e894c4fb
ishavverma <ishu03.ishavverma@gmail.com>
2022-05-22 11:54:36 +0530

    Repository updated.

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-function connect () {
-    mongoose.set('useCreateIndex', true);
-    mongoose.connect('mongodb://localhost/complaintapp',{useNewUrlParser: true})
-}
-
-module.exports = connect
